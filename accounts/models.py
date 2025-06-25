adfa0792d80538f4fd4cb41d0de637cc632b5489
Lekan8562 <jegedeolajide1@gmail.com>
2025-06-25 15:18:59 +0100

    created the first main models

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
+
 class CustomUser(AbstractUser):
-    pass
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+    first_name = models.CharField(max_length=30, null=True, blank=True)
+    last_name = models.CharField(max_length=30, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.email
+        return f"{self.first_name} {self.last_name}"
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 # Create your models here.
+
+class HomePageHero(models.Model):
+    image = models.ImageField(upload_to='hero/images/')
+    description = models.TextField()
+
+    def __str__(self):
+        return self.description
+
+class HomePageBanner(models.Model):
+    image = models.ImageField(upload_to='banner/images')
+    title = models.CharField(max_length=150, null=True, blank=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.title} - {self.description}"
+
+class UnitCodeOfConduct(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Code of Conduct - {self.created_at}"
+
+    class Meta:
+        verbose_name_plural = "Unit Codes of Conduct"
+        ordering = ['-created_at']
+
+
+class Unit(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    mission = models.TextField()
+    coordinator = models.OneToOneField( User, on_delete=models.CASCADE, related_name='unit_coordinator', null=True, blank=True)
+    assistant_coordinator = models.OneToOneField(User, on_delete=models.CASCADE, related_name='unit_assistant_coordinator', null=True, blank=True)
+    members = models.ManyToManyField(User, related_name='unit_members', blank=True)
+    code_of_conduct = models.ForeignKey(UnitCodeOfConduct, on_delete=models.SET_NULL, null=True, blank=True)
+    
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Units"
+        ordering = ['name']
+
